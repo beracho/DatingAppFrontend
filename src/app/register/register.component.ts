f058bdc85087ce8e8a95fbcab71dc7641661e5d4
Adrian Marcelo Berazain Mallea <aberazain@alphasys.com.bo>
2018-06-04 17:41:19 -0400

    a clase 46

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+// import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  model: any = {};
+  @Output() cancelRegister = new EventEmitter();
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('Registration successful');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
   }
 
 }
